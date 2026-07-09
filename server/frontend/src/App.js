806c73400d425f37181557289f5f916651ff8ea8
JJHAVOK <jayemails1996@gmail.com>
2026-07-09 16:42:29 -0400

    Final cleanup of URL routing

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+// Import your other components here
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
