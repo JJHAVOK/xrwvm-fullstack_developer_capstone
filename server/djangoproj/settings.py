788d8254abeeadaca6f8e49906185ad2063194db
JJHAVOK <jayemails1996@gmail.com>
2026-07-09 23:55:24 -0400

    Re-add settings.py with fixed formatting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -145,4 +145,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
+# Use a shorter variable name locally for just this line
+_f = 'frontend/build/static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, _f)]
