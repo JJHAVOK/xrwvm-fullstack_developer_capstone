ad2db6bb7e7f924b1a43a20bf2c7aa482c45402a
JJHAVOK <jayemails1996@gmail.com>
2026-07-09 23:38:21 -0400

    Fix final settings.py line length E501

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -146,5 +146,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(
+        BASE_DIR, 'frontend/build/static'
+    ),
 ]
