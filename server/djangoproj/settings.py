f1f00aab67e8dbfbb34a572d82bdd024d2e57d30
JJHAVOK <jayemails1996@gmail.com>
2026-07-10 00:10:14 -0400

    Force linter to ignore line length on STATICFILES_DIRS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -145,9 +145,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [  # noqa: E501
-    os.path.join(
-        BASE_DIR,
-        'frontend/build/static'
-    ),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]  # noqa: E501
