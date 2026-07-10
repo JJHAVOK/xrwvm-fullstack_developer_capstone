2134ea8df2ad81718a27ed94e364a3afd9c33f8a
JJHAVOK <jayemails1996@gmail.com>
2026-07-10 00:08:14 -0400

    Add noqa ignore to settings.py and restore JS lint path

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -145,7 +145,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = [  # noqa: E501
     os.path.join(
         BASE_DIR,
         'frontend/build/static'
