22d05c166b76db4a824e09b5469ca4fd6a51bbb9
JJHAVOK <jayemails1996@gmail.com>
2026-07-09 23:03:41 -0400

    Cleaned imports and formatting for linter

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,16 +7,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-class CarMake(models.Model):
+class CarMake(models.Model): 
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
 
-    def __str__(self):
+    def __str__(self): 
         return self.name  # Return the name as the string representation
 
 
-class CarModel(models.Model):
+class CarModel(models.Model): 
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
@@ -33,5 +33,5 @@ class CarModel(models.Model):
         ])
     # Other fields as needed
 
-    def __str__(self):
+    def __str__(self): 
         return self.name  # Return the name as the string representation
